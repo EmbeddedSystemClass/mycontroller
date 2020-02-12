bd441d510774b92ca0527e31e7962ee74e904571
Jeeva Kandasamy <jkandasa@gmail.com>
2020-02-12 22:37:02 +0530

    clean

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
-FROM alpine:3.11
-
-LABEL maintainer="Jeeva Kandasamy <jkandasa@gmail.com>"
-
-EXPOSE 8080
-
-COPY mcserver-linux /app/mcserver-linux
-
-ENTRYPOINT ["/app/mcserver-linux"]
+# MyController
--- cmd/main.go
+++ cmd/main.go
@@ -7,13 +7,11 @@ import (
 	storage "github.com/mycontroller-org/mycontroller/pkg/storage"
 )
 
-
-
 func main() {
 	fmt.Println("Welcome to MyController 2.x :)")
 	config := map[string]string{
 		"database": "mydb",
-		"uri":      "mongodb+srv://testuser:testuser@cluster0-sk7af.mongodb.net/test?retryWrites=true&w=majority",
+		"uri":      "test",
 	}
 	err := storage.GetClient(config)
 	if err != nil {
