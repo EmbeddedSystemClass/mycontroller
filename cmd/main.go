7c2b34c6c07b4dcc16b91df26df977ce8f5a29ca
Jeeva Kandasamy <jkandasa@gmail.com>
2020-02-13 00:56:09 +0530

    configuration

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,50 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
 
 	"github.com/mycontroller-org/mycontroller/cmd/app/handler"
 	"github.com/mycontroller-org/mycontroller/pkg/storage"
 )
 
+// WebConfig input
+type WebConfig struct {
+	BindAddress  string `yaml:"bindAddress"`
+	Port         uint   `yaml:"port"`
+	WebDirectory string `yaml:"webDirectory"`
+}
+
+// Config of the system
+type Config struct {
+	Database map[string]string `yaml:"database"`
+	Web      WebConfig         `yaml:"web"`
+}
+
 func main() {
 	fmt.Println("Welcome to MyController 2.x :)")
-	config := map[string]string{
-		"database": "mydb",
-		"uri":      "mongodb+srv://testuser:testuser@cluster0-sk7af.mongodb.net/test?retryWrites=true&w=majority",
+	cf := flag.String("config", "./config.yaml", "Configuration file")
+	flag.Parse()
+	if cf == nil {
+		panic(errors.New("Configuration file not supplied"))
 	}
-	err := storage.Init(config)
+	fmt.Println(flag.Args())
+	fmt.Println("File Name:", *cf)
+	d, err := ioutil.ReadFile(*cf)
+	if err != nil {
+		panic(err)
+	}
+	var config Config
+	yaml.Unmarshal(d, &config)
+	db := map[string]string{
+		"database": config.Database["name"],
+		"uri":      config.Database["uri"],
+	}
+	err = storage.Init(db)
 	if err != nil {
 		panic(err)
 	}
--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/google/uuid v1.1.1
 	github.com/rs/cors v1.7.0
 	go.mongodb.org/mongo-driver v1.3.0
+	gopkg.in/yaml.v2 v2.2.2
 )
